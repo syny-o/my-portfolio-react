83742095c71a24b9894fd1490af06d3a87ce3890
syny-o <synek.o@seznam.cz>
2024-11-29 20:32:03 +0100

    Created component SocialBox

---
+++ src/components/Shared/SocialBox.jsx
@@ -0,0 +1,19 @@
+import "./SocialBox.css";
+
+function SocialBox() {
+  return (
+    <div className="social">
+      <div className="social-image-container">
+        <img src="/assets/social/mail.png" alt="" />
+      </div>
+      <div className="social-image-container">
+        <img src="/assets/social/github.png" alt="" />
+      </div>
+      <div className="social-image-container">
+        <img src="/assets/social/linkedin.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default SocialBox;

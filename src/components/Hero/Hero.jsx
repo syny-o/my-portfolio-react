b5a30b7fa16f1a5024126716cef72107edd9e555
syny-o <synek.o@seznam.cz>
2024-12-03 17:09:44 +0100

    stars component created

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,21 +5,13 @@ import { HiDownload } from "react-icons/hi";
 import "./Hero.css";
 import Carousel from "./Carousel";
 import Button from "../Shared/Button";
+import Stars from "./Stars";
 
 function Hero() {
   return (
     <section className="hero-container">
       <div className="content">
-        <div class="shape shape1"></div>
-        <div class="shape shape2"></div>
-        <div class="shape shape3"></div>
-        <div class="shape shape4"></div>
-        <div class="shape shape5"></div>
-        <div class="shape shape6"></div>
-        <div class="shape shape7"></div>
-        <div class="shape shape8"></div>
-        <div class="shape shape9"></div>
-        <div class="shape shape10"></div>
+        <Stars />
         <div className="top-blur"></div>
         <div className="avatar-overlay">
           <img className="avatar" src="/assets/avatar/syny.png" alt="" />
@@ -50,19 +42,9 @@ function Hero() {
         />
       </div>
 
-      {/* <img
-        className="hero-img"
-        // src="/assets/hero/heroImage.png"
-        src="/assets/projects/r2editor.png"
-        alt="hero-image"
-      /> */}
-
       <div className="carousel">
         <Carousel />
       </div>
-
-      {/* <div className="top-blur"></div> */}
-      {/* <div className="bottom-blur"></div> */}
     </section>
   );
 }
---
+++ src/components/Hero/Stars.jsx
@@ -0,0 +1,20 @@
+import "./Stars.css";
+
+function Stars() {
+  return (
+    <>
+      <div class="shape shape1"></div>
+      <div class="shape shape2"></div>
+      <div class="shape shape3"></div>
+      <div class="shape shape4"></div>
+      <div class="shape shape5"></div>
+      <div class="shape shape6"></div>
+      <div class="shape shape7"></div>
+      <div class="shape shape8"></div>
+      <div class="shape shape9"></div>
+      <div class="shape shape10"></div>
+    </>
+  );
+}
+
+export default Stars;

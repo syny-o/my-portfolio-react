38a53b6808da1c8e94e3ba331cb896ba14e39ec7
syny-o <synek.o@seznam.cz>
2024-12-11 13:25:19 +0100

    fixed syntax

--- src/components/Hero/Stars.jsx
+++ src/components/Hero/Stars.jsx
@@ -3,16 +3,16 @@ import "./Stars.css";
 function Stars() {
   return (
     <>
-      <div class="shape shape1"></div>
-      <div class="shape shape2"></div>
-      <div class="shape shape3"></div>
-      <div class="shape shape4"></div>
-      <div class="shape shape5"></div>
-      <div class="shape shape6"></div>
-      <div class="shape shape7"></div>
-      <div class="shape shape8"></div>
-      <div class="shape shape9"></div>
-      <div class="shape shape10"></div>
+      <div className="shape shape1"></div>
+      <div className="shape shape2"></div>
+      <div className="shape shape3"></div>
+      <div className="shape shape4"></div>
+      <div className="shape shape5"></div>
+      <div className="shape shape6"></div>
+      <div className="shape shape7"></div>
+      <div className="shape shape8"></div>
+      <div className="shape shape9"></div>
+      <div className="shape shape10"></div>
     </>
   );
 }
